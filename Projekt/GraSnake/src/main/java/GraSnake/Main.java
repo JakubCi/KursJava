34d2de58e8cffcb7eec686842b195ada3f054daa
JakubCi <90244126+JakubCi@users.noreply.github.com>
2021-10-27 21:54:12 +0200

    Add files via upload

---
+++ Projekt/GraSnake/src/main/java/GraSnake/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package GraSnake;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    public Main() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Main();
+            ex.setVisible(true);
+        });
+    }
+}
